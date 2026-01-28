4a9703263babd234ce2e7824cef7ce7e2d49b455
Cinthia <cinthia.parra.solorio@outlook.com>
2026-01-27 20:26:05 -0600

    add homework

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
-//Este es un comentario en JavaScript 
+//Este es un comentario en JavaScript
 
-//Tipos de datos en JavaScript 
+//Tipos de datos en JavaScript
 
-//Number: enteros y descimales 
+//Number: enteros y descimales
 let edad = 24;
 let precio = 199.99;
- console.log("Number:");
- console.log(edad);
- console.log(precio);
+console.log("Number:");
+console.log(edad);
+console.log(precio);
 
- console.log(typeof edad); 
- console.log(typeof precio);
+console.log(typeof edad);
+console.log(typeof precio);
 
- //String: cadenas de texto 
- let nombre ='Ana'; 
- let saludo = "Hola mundo"; 
+//String: cadenas de texto
+let nombre = "Ana";
+let saludo = "Hola mundo";
 
- console.log("String:");
- console.log(nombre);
- console.log(saludo);
- console.log(typeof nombre);
- console.log(typeof saludo);
+console.log("String:");
+console.log(nombre);
+console.log(saludo);
+console.log(typeof nombre);
+console.log(typeof saludo);
 
- //Boolean: true o false
- let esMayorDeEdad = true;
- let tienePermiso = false;
+//Boolean: true o false
+let esMayorDeEdad = true;
+let tienePermiso = false;
 
- console.log("Type of data Boolean:");
- console.log(esMayorDeEdad);
- console.log(tienePermiso);
- console.log(typeof esMayorDeEdad);
+console.log("Type of data Boolean:");
+console.log(esMayorDeEdad);
+console.log(tienePermiso);
+console.log(typeof esMayorDeEdad);
 
- //Undefined: variable declarada pero sin valor asignado
- let telefono;
- console.log("Type of data Undefined:");
- console.log(telefono);
- console.log(typeof telefono);
+//Undefined: variable declarada pero sin valor asignado
+let telefono;
+console.log("Type of data Undefined:");
+console.log(telefono);
+console.log(typeof telefono);
 
- //Null: ausencia intencional de valor
- let Direccion = null;
- console.log("Type of data Null:");
- console.log(Direccion);
- console.log(typeof Direccion); //Resultado sera object, es un error historico de JavaScript (no es igual a object pero nunca lo cambiaron para que no chashe lo existente)
+//Null: ausencia intencional de valor
+let Direccion = null;
+console.log("Type of data Null:");
+console.log(Direccion);
+console.log(typeof Direccion); //Resultado sera object, es un error historico de JavaScript (no es igual a object pero nunca lo cambiaron para que no chashe lo existente)
 
- //Object: coleccion de dato (estructura de datos con propiedades)
+//Object: coleccion de dato (estructura de datos con propiedades)
 let persona = {
-    nombre: 'Diego',
+    nombre: "Diego",
     edad: 30,
-    ciudad: 'Madrid',
+    ciudad: "Madrid",
 };
 
 console.log("Type of data Object:");
@@ -55,7 +55,14 @@ console.log(typeof persona);
 console.log(persona);
 
 //symbol: valor unico e inmutable
-let id =Symbol('id');
+let id = Symbol("id");
 console.log("Type of data Symbol:");
 console.log(id);
 console.log(typeof id);
+
+//buenas practicas al nombrar variables
+let apellido = 34;
+let cumpleanos = "zapopan";
+
+let a = "pera"; //no es buena practica
+let b = 45; //no es buena practica
---
+++ type-of-data.js
@@ -0,0 +1,45 @@
+
+// 42
+let numero1 = 42;
+console.log(numero1);
+console.log(typeof numero1);
+
+// 'Veinticinco'
+let numeSring = 'Veinticinco';
+console.log(numeSring);
+console.log(typeof numeSring);
+
+// -666
+let numeroNegativo = -666;
+console.log(numeroNegativo);
+console.log(typeof numeroNegativo);
+
+// true
+let booleano = true;
+console.log(booleano);
+console.log(typeof booleano); 
+
+// 0
+let cero = 0;
+console.log(cero);
+console.log(typeof cero);
+
+// ''
+let stringVacio = '';
+console.log(stringVacio);
+console.log(typeof stringVacio);   
+
+// null
+let valorNulo = null;
+console.log(valorNulo);
+console.log(typeof valorNulo);
+
+// undefined
+let valorIndefinido = undefined;
+console.log(valorIndefinido);
+console.log(typeof valorIndefinido);
+
+// FALSE
+let booleanoFalso = false;
+console.log(booleanoFalso);
+console.log(typeof booleanoFalso);
