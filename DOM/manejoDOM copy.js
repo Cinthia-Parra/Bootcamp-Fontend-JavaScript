bcbaef6551ace828213f05b96acb1e5e4539ac5b
Cinthia <cinthia.parra.solorio@outlook.com>
2026-02-12 20:37:11 -0600

    get manejo DOM

---
+++ DOM/manejoDOM copy.js
@@ -0,0 +1,8 @@
+// Problema: Caja de Comentarios (Simple Comment Box)
+// Crear una caja de comentarios donde los usuarios puedan escribir y agregar comentarios que se muestran en la página. Este proyecto ayudará a los estudiantes a manejar eventos de formularios y modificar el contenido de una página.
+
+// Instrucciones para resolver el problema:
+// Crea una página con un formulario que tenga un campo de texto para el comentario y un botón para agregarlo.
+// Cuando el usuario haga clic en el botón, el comentario debe aparecer en la página debajo del formulario.
+// Los estudiantes pueden agregar un botón para eliminar comentarios si lo desean.
+// Los comentarios deben aparecer junto con la fecha y hora de publicación. (opcional)
